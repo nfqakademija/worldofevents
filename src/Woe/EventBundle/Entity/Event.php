fb198e516f1e65f87c799b5a05cb7110de4b9367
Daniil Pyšniak <dpk125@users.noreply.github.com>
2014-11-06 17:18:17 +0200

    Added Event entity

---
+++ src/Woe/EventBundle/Entity/Event.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Woe\EventBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Woe\EventBundle\Entity\EventRepository")
+ */
+class Event
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price_min", type="decimal")
+     */
+    private $priceMin;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price_max", type="decimal")
+     */
+    private $priceMax;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="information", type="text")
+     */
+    private $information;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Event
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set priceMin
+     *
+     * @param string $priceMin
+     * @return Event
+     */
+    public function setPriceMin($priceMin)
+    {
+        $this->priceMin = $priceMin;
+
+        return $this;
+    }
+
+    /**
+     * Get priceMin
+     *
+     * @return string 
+     */
+    public function getPriceMin()
+    {
+        return $this->priceMin;
+    }
+
+    /**
+     * Set priceMax
+     *
+     * @param string $priceMax
+     * @return Event
+     */
+    public function setPriceMax($priceMax)
+    {
+        $this->priceMax = $priceMax;
+
+        return $this;
+    }
+
+    /**
+     * Get priceMax
+     *
+     * @return string 
+     */
+    public function getPriceMax()
+    {
+        return $this->priceMax;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Event
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set information
+     *
+     * @param string $information
+     * @return Event
+     */
+    public function setInformation($information)
+    {
+        $this->information = $information;
+
+        return $this;
+    }
+
+    /**
+     * Get information
+     *
+     * @return string 
+     */
+    public function getInformation()
+    {
+        return $this->information;
+    }
+}
