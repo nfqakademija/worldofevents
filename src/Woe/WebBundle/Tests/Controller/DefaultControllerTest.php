e79cd1d903d50f36aea2fbfb423206d8dec83855
Daniil Pyšniak <dpk125@users.noreply.github.com>
2014-11-28 16:17:46 +0200

    Fixed failing Hello World test

--- src/Woe/WebBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Woe/WebBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello World")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("World of Events")')->count() > 0);
     }
 }
