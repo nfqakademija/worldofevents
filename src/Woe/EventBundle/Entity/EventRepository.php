996bd51792c4ef47bc4554e5680adf35d1612142
Daniil Pyšniak <dpk125@users.noreply.github.com>
2014-12-04 17:04:42 +0200

    Changed events search query, which returns an event only if all search terms are matched

--- src/Woe/EventBundle/Entity/EventRepository.php
+++ src/Woe/EventBundle/Entity/EventRepository.php
@@ -17,7 +17,12 @@ class EventRepository extends EntityRepository
         $query = $this->createQueryBuilder('event')
             ->innerJoin('event.keywords', 'k')
             ->where('k.name IN (:keywords)')
-            ->setParameter('keywords', $keywords);
+            ->groupBy('event.id')
+            ->having("COUNT(event.id) = :count")
+            ->setParameters(array(
+                'keywords' => $keywords,
+                'count' => count($keywords)
+            ));
 
         return $query->getQuery()->getResult();
     }
