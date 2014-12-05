6ded054fe3d5dd321afbd172b6a6a8c7c54e187b
Daniil Pyšniak <dpk125@users.noreply.github.com>
2014-12-05 22:34:27 +0200

    Moved SensioGeneratorBundle to prod env

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,11 +23,11 @@ class AppKernel extends Kernel
             new Woe\MapperBundle\WoeMapperBundle(),
             new Bazinga\Bundle\GeocoderBundle\BazingaGeocoderBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
